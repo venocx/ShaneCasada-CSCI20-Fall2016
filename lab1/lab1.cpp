da363d7941ffa8b219cfb0a28f826dda2a0c5151
Shane Casada <venocx@yahoo.com>
2016-08-25 21:13:31 +0000

    1.1 Lab - ASCII Art

---
+++ lab1/lab1.cpp
@@ -0,0 +1,24 @@
+//Shane Casada
+//8/25/16 
+
+# include <iostream>
+using namespace std;
+    
+int main()
+    {     
+        cout<<"               "<<endl;
+        cout<<"       ( )     "<<endl;
+        cout<<"       |=|     "<<endl;
+        cout<<"       |=|     "<<endl;
+        cout<<"     __|_|__   "<<endl;
+        cout<<"   (   ( )   ) "<<endl;
+        cout<<"     |  Y  |   "<<endl;
+        cout<<"     |  |  |   "<<endl;            
+        cout<<"     |  |  |   "<<endl;            
+        cout<<"     |  |  |   "<<endl;          
+        cout<<"     |  |  |   "<<endl;        
+        cout<<"     \\  |  /   "<<endl;
+        cout<<"      \\   /    "<<endl;
+        cout<<"       \\ /     "<<endl;
+       }
+
