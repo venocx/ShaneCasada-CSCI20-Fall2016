b1eab186259c3ca6ea4b778919d2f99485f6177b
Shane Casada <venocx@yahoo.com>
2016-08-26 02:44:43 +0000

    Lab2 submission

---
+++ lab2/lab2.cpp
@@ -0,0 +1,14 @@
+//1. Add a comment with name, date, discription of program
+//2. Add rand to # include <random> 
+//3. Add using namespace std;
+//4. Add int main() {
+//5. In main add the code for num gen that alows to pick between two numbers lets say 0-10 .
+//6. Then add the code that allows the user to guess the two numbers.
+//7. add a point system.
+//8. If they don’t the program tells them either higher or lower and lets them guess until they win and lose 1 point from 10 for each incorrect guess.
+//7. Then add an if else statement that alows the user to continue the game if picked the bad number. And says to player “higher” or “lower” as well as takes 1 point from 10 for each incorrect guess.
+//8. Need a loop to go back to the start with that guessed number out or the picked numbers as well as lost a point.
+//9. Need a a line of code to say that the player already picked that number and to pick again.  
+//8. add if else statment that allows the num gen to end if picked right and restart.
+//9. end int main with } 
+
