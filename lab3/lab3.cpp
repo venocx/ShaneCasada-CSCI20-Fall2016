752b6ef11b9923192284dd44ebf4408c6f102963
Shane Casada <venocx@yahoo.com>
2016-08-31 03:18:02 +0000

    lab3 submission

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -8,19 +8,17 @@ using namespace std;
 
 int main()
 {
+    // added cout.flush() to flush the cout so I could use 
     int number=0; //number is Integer
     cout<<"Please enter a year: ";
-    cout.flush();
     cin >> number;
     
     int number3=0; //number is Integer
     cout<<"Please enter another year: ";
-    cout.flush();
     cin >> number3;
     
     float number2=0; //number2 is a Floating point
     cout<<"Please enter a number with a decimal point:";
-    cout.flush();
     cin>>number2;
     
     string noun; 
@@ -29,26 +27,21 @@ int main()
     getline(cin.ignore(), noun);
  
     
-    
     string noun2; 
     cout<<"Please enter another name: ";;
-    cout.flush();
     getline(cin, noun2);
     
     string job;
     cout<<"Please enter a job title: ";
-    cout.flush();
     getline(cin, job);
    
     
     string job2;
     cout<<"Please enter another job title: ";
-    cout.flush();
     getline(cin, job2);
     
     string prize;
     cout<<"Please enter yet another job title: ";
-    cout.flush();
     getline(cin, prize);
     
     cout<<"Albert Einstein, the son of "<<noun<< " and "<<noun2<<" was boarn in "<<number3<<"."
