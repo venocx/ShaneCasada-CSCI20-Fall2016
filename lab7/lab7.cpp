f573b7fab6096dd65d199bd6db03ccc08e74872e
Shane Casada <venocx@yahoo.com>
2016-09-22 21:07:38 +0000

    lab7 submission

--- lab7/lab7.cpp
+++ lab7/lab7.cpp
@@ -14,33 +14,36 @@
 
 using namespace std;
 
-//global variables so every function can use it.
- int x = 99;
+
+
  
-void forloop(){
+int min_bottle(int x)
+{
     
-    for (x; x = 0 ; x--){
-    }
+    x--;
+       
+    return x;
     
-}
- 
-void bottleCount(int x, int y){
-    
-       x = x - y;
- 
 }
 
-void song(){
-    
-   
-    cout << x << " bottles of beer on the wall, " << x << " bottles of beer. Take one down and pass it around," << x << " bottles of beer on the wall." << endl;
+
+string nothing(string y, string z)
+{
+    return y, z;
 }
  
-// I love how clean the main looks. It makes my 💀 all nice and hard.
-int main() { 
+
+int main() 
+{
+    string z = " ";
+    string y = " ";
     
-    bottleCount(99, 0);
-    song();
+for(int x = 100; x >= 2; x--)
+  {
+      cout << min_bottle(x) <<" bottles of beer on the wall, " << min_bottle(x) << " bottles of beer. Take one down and pass it around, " << (min_bottle(x)-1) << " bottles of beer on the wall." << nothing(y, z) << endl;
+  }
+    
+  
    
     return 0;
 }
